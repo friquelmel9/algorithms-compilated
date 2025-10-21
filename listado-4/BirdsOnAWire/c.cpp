cf3b7036f01e97273fe9df0595284b41e2e2ea1a
friquelmel9 <friquelmel.137@gmail.com>
2025-10-21 07:56:19 -0300

    feat(listado-4): solution for Birds on a Wire

--- listado-4/BirdsOnAWire/c.cpp
+++ listado-4/BirdsOnAWire/c.cpp
@@ -7,45 +7,34 @@ int main(){
     ll l,d,n;
     std::cin >> l >> d >> n;
     
-    // case 1: pole too short
-    if(l <= 12){ 
-        std::cout << 0 << std::endl;
-        return 0;
-    }
-    
-    // case 2: no birds on wire
-    if(n==0){
-        std::cout << (l-12)/d << std::endl;
-        return 0;
-    }
-    
-    // case 3: birds on wire
-    std::vector<ll> positions; // works like nodes
-    
-    for(ll i=0; i<n; i++){
-        
-        ll input;
+    std::vector<ll> positions;
+    ll input;
+    for(int i=0; i<n; i++){
         std::cin >> input;
         positions.push_back(input);
-        
     }
     
-    positions.push_back(6);
-    std::sort(positions.begin(), positions.end());
-    positions.push_back(l-5);
+    if(n>0){std::sort(positions.begin(), positions.end());}
     
     ll new_birds = 0;
     
-    for(ll i=0; i<n+1; i++){
+    // case birds
+    for(ll i=0; i<n-1; i++){
         
         ll pos1 = positions[i];
         ll pos2 = positions[i+1];
         
-        new_birds += (pos2-pos1-1)/2;
-        
+        new_birds += std::max((pos2-pos1)/d - 1, 0ll);
     }
     
-    std::cout << new_birds << std::endl;
+    // edge cases
+    if(n > 0){
+        new_birds += (positions[0]-6)/d;
+        new_birds += (l-6-positions[n-1])/d;
+    }
+    else if(l >= 12) // no birds but string
+        new_birds += (l-12)/d + 1;
     
+    std::cout << new_birds << std::endl;
     return 0;
 }
